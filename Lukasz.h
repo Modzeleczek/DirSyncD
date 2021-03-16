54c9bcf0844c9e8a5d8fbb67f216116b10934ed7
Łukasz Modzelewski <lukasz.modzelewski1@gmail.com>
2021-03-17 00:19:53 +0100

    Add parseParameters signature.

--- Lukasz.h
+++ Lukasz.h
@@ -2,6 +2,21 @@
 #ifndef LUKASZ_H
 #define LUKASZ_H
 
+/*
+odczytuje:
+argc - liczba parametrów programu (opcji i argumentów razem)
+argv - parametry programu
 
+zapisuje:
+source - ścieżka źródłowa
+destination - ścieżka docelowa
+interval - czas spania w sekundach
+recursive - rekurecyjna synchronizacja katalogów
+
+zwraca:
+< 0, jeżeli wystąpił błąd
+0, jeżeli nie wystąpił błąd
+*/
+int parseParameters(int argc, char **argv, char **source, char **destination, unsigned int *interval, char *recursive);
 
 #endif
